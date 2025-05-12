0c88772f56709875f343f441cbc166ee6f277eba
naong <naong4ha@gmail.com>
2025-05-12 23:35:57 +0900

    fix : 로그인 dto에 user_id가 null로 되어있던거 삭제.

--- src/main/java/com/foodu/Membership/Dto/LoginResponse.java
+++ src/main/java/com/foodu/Membership/Dto/LoginResponse.java
@@ -6,16 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class LoginResponse {
-
-    private Integer userId;
     private String token;
     private String role;
     private String message;
-
-    public LoginResponse(String token, String role, String message) {
-        this.userId = null;  // 또는 -1, 또는 의미 없는 값
-        this.token = token;
-        this.role = role;
-        this.message = message;
-    }
 }
